5d5d2136287fb4848e0ea1f57c1f0a790c87c608
LyubakaT <mrltonchev@gmail.com>
2016-09-24 12:37:53 +0300

    added some stuff

---
+++ Draw-with-Loops/Diamond/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var mid = -1;
+
+            if (n % 2 == 0)
+            {
+                mid = 0; 
+            }
+            for (int i = 1; i <= (n+1) / 2; i++)
+            {
+                var left = (n - 2 - mid) / 2;
+
+                Console.Write(new string ('-', left));
+                Console.Write("*");
+                if (mid >= 0)
+                {
+                    Console.Write(new string('-', mid));
+                    Console.Write("*");
+                }
+                Console.Write(new string('-', left));
+                Console.WriteLine();
+                mid = mid + 2;
+            }
+            //Console.WriteLine("разделител");
+            mid = n - 4;
+            for (int i = 1; i < (n + 1) / 2; i++)
+            {
+                var left = (n - 2 - mid) / 2;
+
+                Console.Write(new string('-', left));
+                Console.Write("*");
+                if (mid >= 0)
+                {
+                    Console.Write(new string('-', mid));
+                    Console.Write("*");
+                }
+                Console.Write(new string('-', left));
+                Console.WriteLine();
+                mid = mid - 2;
+            }
+        }    
+    }
+}
