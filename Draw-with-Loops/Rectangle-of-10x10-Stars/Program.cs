b18fe69be42d30825c22e703a6b82e981562afdc
Lyuben Tonchev <mrltonchev@gmail.com>
2016-09-24 23:33:04 +0300

    Update Program.cs

--- Draw-with-Loops/Rectangle-of-10x10-Stars/Program.cs
+++ Draw-with-Loops/Rectangle-of-10x10-Stars/Program.cs
@@ -12,7 +12,7 @@ namespace Rectangle_of_10x10_Stars
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(new string ('*',  10));  
+          
             }
         }
     }
